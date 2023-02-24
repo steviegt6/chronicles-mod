3aaca1bfed342c01cb82b0e358072dab732ef585
Tomat <xxlennygamerxx@gmail.com>
2023-02-23 19:39:05 -0800

    Begin doing stuff.

--- Chronicles.cs
+++ Chronicles.cs
@@ -1,11 +1,29 @@
+using System;
 using JetBrains.Annotations;
 using Terraria.ModLoader;
 
 namespace Chronicles;
 
+/// <summary>
+///     The central <see cref="Mod"/> class for Chronicles. Manages very little
+///     information by itself, but is employed heavily by the tModLoader API.
+/// </summary>
+/// <remarks>
+///     This class should be responsible for very little. Information and logic
+///     should be delegated to various <see cref="ModSystem"/>s instead (or
+///     elsewhere when appropriate).
+/// </remarks>
 [UsedImplicitly(ImplicitUseKindFlags.InstantiatedWithFixedConstructorSignature)]
 public sealed class ChroniclesMod : Mod {
+    private static bool instantiated;
+
     public ChroniclesMod() {
-        _ = 0;
+        // Shouldn't generally ever happen, but I'm paranoid - especially about
+        // the logic behind assembly loading and unloaded. Better safe than
+        // sorry, yeah?
+        if (instantiated)
+            throw new InvalidOperationException($"Attempted to instantiate a second instance of {nameof(ChroniclesMod)} in the same AssemblyLoadContext.");
+
+        instantiated = true;
     }
 }
---
+++ Core/Systems/BrandingCreditsLogger.cs
@@ -0,0 +1,22 @@
+using JetBrains.Annotations;
+using Terraria.ModLoader;
+
+namespace Chronicles.Core.Systems;
+
+[UsedImplicitly(ImplicitUseKindFlags.InstantiatedWithFixedConstructorSignature)]
+internal sealed class BrandingCreditsLogger : ModSystem {
+    public override void OnModLoad() {
+        base.OnModLoad();
+
+        void log(string msg) {
+            Mod.Logger.Info(msg);
+        }
+        
+        log($"{Mod.DisplayName} ({Mod.Name}) v{Mod.Version}");
+        log("by Tomat, ZENISIS, and Lunin, with help from any contributors.");
+        log("");
+        log("Assets licensed under All Rights reserved, code licensed under the GNU General Public License, version 3.0.");
+        log("FOSS @ <https://github.com/steviegt6/chronicles-mod>");
+        log("See COPYING in the root directory of the repository for further licensing information.");
+    }
+}
