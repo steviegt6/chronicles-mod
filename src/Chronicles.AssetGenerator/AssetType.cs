bf0d1413b4c110600ea5fbed713293af8e9c8df4
Tomat <xxlennygamerxx@gmail.com>
2023-04-03 23:04:29 -0700

    Rewrite asset generator to be better.

---
+++ src/Chronicles.AssetGenerator/AssetType.cs
@@ -0,0 +1,16 @@
+﻿namespace Chronicles.AssetGenerator;
+
+/// <summary>
+///     Distinguishes asset types.
+/// </summary>
+public enum AssetType {
+    /// <summary>
+    ///     <c>.png</c>
+    /// </summary>
+    Image,
+    
+    /// <summary>
+    ///     Unknown asset type, errors.
+    /// </summary>
+    Unknown,
+}
